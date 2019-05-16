7fcb7c28d6f2a47f6243f9cfa486ea23464c28c5
fansi-sifan <sifan1121@gmail.com>
2019-05-16 16:52:16 -0400

    match place to msa

--- analysis.R
+++ analysis.R
@@ -1,4 +1,4 @@
-pkgs <- c('tidyverse','tidytext',"SifanLiu","network","metro.data")
+pkgs <- c('tidyverse','tidytext',"SifanLiu","network","metro.data","plotly")
 
 check <- sapply(pkgs,require,warn.conflicts = TRUE,character.only = TRUE)
 if(any(!check)){
@@ -12,19 +12,11 @@ if(any(!check)){
 cb_all <- read_all("../Datasets/innovation/crunchbase/data")
 # select active companies only
 df <- cb_all%>%
-  filter(Operating.Status=="Active")
+  filter(Operating.Status == "Active")
 
-# to do
-# match cities to MSA
 # read xwalk
 # create standardized columns in adition to original columns
-pl2co <- place2county  %>%
-  mutate(pl_label = gsub(" city| town| CDP| village| municipality| borough", "", pl_name),
-         pl_label = tolower(gsub("\\,.+", "", pl_label)),
-         pl_label = ifelse(grepl("(balance)",pl_label),
-                             gsub(" \\(balance\\)","",gsub(" \\w.+|\\-\\w.+","",pl_label)),
-                             pl_label),
-         st_name = state.name[match(st_ab,state.abb)])
+
 
 place <- df %>% select(Headquarters.Location)%>%
   separate(Headquarters.Location,c("pl_name",'st_name',"US"),sep = ",",remove = F)%>%
@@ -35,15 +27,40 @@ place <- df %>% select(Headquarters.Location)%>%
                              pl_label))%>%
   unique()
 
-tmp <- place %>%
-  left_join(pl2co[c("pl_label","st_name","stcofips","afact1")], by = c("pl_label","st_name"))%>%
-  group_by(Headquarters.Location,pl_label,st_name)
+matched <- place %>%
+  left_join(pl2co[c("pl_label","st_name","stco_fips","afact1")], by = c("pl_label","st_name"))%>%
+  filter(!is.na(stco_fips))
 
+unmatched <- place %>%
+  left_join(pl2co[c("pl_label","st_name","stco_fips","afact1")], by = c("pl_label","st_name"))%>%
+  filter(is.na(stco_fips))
+
+# use geocoding api
+KEY <- Sys.getenv("GOOGLE_MAP_KEY")
+
+for (i in 1:nrow(unmatched)) {
+  unmatched$stcobk_fips[[i]] <- add2FIPS(unmatched$Headquarters.Location[[i]], KEY)
+}
+
+
+# update place
+place <- bind_rows(matched,
+                   unmatched %>%
+                     mutate(stco_fips = str_sub(unmatched$stcobk_fips,1,5))%>%
+                     select(-stcobk_fips))
+
+# update the xwalk
+pl2fips <- bind_rows(pl2co,
+                     unmatched %>%
+                       mutate(stco_fips = str_sub(unmatched$stcobk_fips,1,5))%>%
+                       select(-Headquarters.Location,-US))
+
+save(pl2fips, file = "V:/Sifan/SifanLiu/data/pl2fips.rda")
 
 # merge back
 df<- df %>%
-  left_join(tmp, by = "Headquarters.Location")%>%
-  left_join(county_cbsa_st, by = c("stcofips"="stco_fips"))
+  left_join(place, by = "Headquarters.Location")%>%
+  left_join(county_cbsa_st, by = "stco_fips")
 
 
 # aggregate by city
@@ -77,15 +94,20 @@ cb_city <- unnest_tokens(df, out, Categories, token = "regex", pattern =",")%>%
 tmp <- cb_city%>%
   filter(lq > 1)%>%
   mutate(RTA = 1)%>%
-  select(msa = cbsa_name,
+  select(cbsa_name,cbsa_code,
          out,RTA)%>%
-  group_by(msa)%>%
+  group_by(cbsa_name,cbsa_code)%>%
   mutate(diversity = n())%>%
   group_by(out)%>%
   mutate(ubiquity = n())%>%
   ungroup()%>%
-  group_by(msa)%>%
+  group_by(cbsa_name,cbsa_code)%>%
   summarise(mean_ubi = mean(ubiquity),
-            diversity = mean(diversity))
+            diversity = mean(diversity))%>%
+  filter(cbsa_code%in%get_code_cbsa100())
 
 plot(tmp$diversity,tmp$mean_ubi)
+
+p <- ggplot(tmp, aes(x = diversity, y=mean_ubi, label = cbsa_name))+ geom_point(stat = "identity")
+
+ggplotly(p)
