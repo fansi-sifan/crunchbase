eeb2005c188f424a87c9a471bce9958e96f38716
fansi-sifan <sifan1121@gmail.com>
2019-06-14 16:33:15 -0400

    clean crunchbase categories

---
+++ categories.R
@@ -0,0 +1,53 @@
+# Author: Sifan Liu
+# Date: Wed Jun 12 15:34:28 2019
+# --------------
+pkgs <- c('tidyverse')
+
+check <- sapply(pkgs,require,warn.conflicts = TRUE,character.only = TRUE)
+if(any(!check)){
+    pkgs.missing <- pkgs[!check]
+    install.packages(pkgs.missing)
+    check <- sapply(pkgs.missing,require,warn.conflicts = TRUE,character.only = TRUE)
+} 
+
+# FAQ
+# https://support.crunchbase.com/hc/en-us/articles/360009616373
+
+# clean categories
+cat <- read_csv("categories-6-12-2019.csv") 
+
+# cat %>% group_by(`Category Groups`)%>%
+#   count()
+
+# extrat unique category group as cat_broad
+cat_broad <- cat$`Category Groups`%>%
+  str_split(",")%>% 
+  unlist()%>%
+  trimws()%>%
+  unique()
+
+# write.csv(cat_broad,"cat_broad.csv")
+cat_tech <- read.csv("cat_broad.csv") %>%
+  mutate(group = tolower(trimws(group)))
+
+cat_token <- cat %>%
+  filter(!is.na(`Category Groups`))%>%
+  filter(!is.na(`Category Name`))%>%
+  
+  # identify tech groups
+  unnest_tokens(group,`Category Groups`, token = "regex", pattern = ",") %>%
+  mutate(group = tolower(trimws(group)))%>%
+  left_join(cat_tech, by = "group") %>%
+  mutate(is_tech = str_length(tech_type)>1)%>%
+  group_by(`Category Name`)%>%
+  summarise(tech = mean(is_tech))%>%
+  mutate(not_tech = tech == 0) %>%
+  # remove broad names
+  mutate(name = tolower(trimws(`Category Name`)),
+         is_broad = `Category Name` %in% cat_broad) %>%
+  mutate(is_broad = ifelse(name %in% c("internet"), TRUE,is_broad))%>%
+  unique() %>%
+  mutate(rm = not_tech | is_broad)
+
+save(cat_name,file = "data/cat_name.rda")
+save(cat_token, file = "data/cat_token.rda")
