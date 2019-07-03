481dd7bc71482d7551a520e5e820a124530456dd
Sifan Liu <SLiu@brookings.edu>
2019-07-04 08:03:19 +1100

    use rcrunchbase package

--- fetch.R
+++ fetch.R
@@ -11,14 +11,13 @@ key <- Sys.getenv("CRUNCHBASE_KEY")
 query <- "https://api.crunchbase.com/v3.1/organizations?user_key="
 query <- "https://api.crunchbase.com/v3.1/organizations/facebook?relationships=funding_rounds,investors&user_key="
 
-query <- "https://api.crunchbase.com/v3.1/organizations/facebook?relationships=funding_rounds,investors&user_key="
-
+query <- "https://api.crunchbase.com/v3.1/odm-organizations?locations=US&organization_type=company&user_key="
 
 
 # temp <- GET(paste0(query, key))
 df <- fromJSON(paste0(query,key))
 
-str(df$data$properties)
+str(df$data$items)
 
 # FETCH FUNCTIONS ========================
 
@@ -27,3 +26,8 @@ str(df$data$properties)
 # FETCH DATA =============================
 
 # SAVE ===================================
+
+# try rcrunchbase packsge --------
+# http://htmlpreview.github.io/?https://github.com/tarakc02/rcrunchbase/blob/master/vignettes/getting-started.html
+library(rcrunchbase)
+rcrunchbase::crunchbase_expand_section()
