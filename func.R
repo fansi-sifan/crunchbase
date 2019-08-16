d39775d01f1dd4eb1d20cfa88842e445dafb3537
fansi-sifan <sifan1121@gmail.com>
2019-08-16 17:38:32 -0400

    new function: clean_cols, define_startups

--- func.R
+++ func.R
@@ -1,3 +1,51 @@
+library(lubridate)
+library(tidyverse)
+library(skimr)
+
+load("cb_us_companies.rda")
+
+clean_cols <- function(df){
+  df %>%
+  select(permalink, name, city_name, region_name, cat_detail, stock_exchange,stock_symbol,
+         founded_year, funding_year, funding_type, isclosed,closed_year)%>%
+    separate(funding_type, "funding_type_latest", sep = ",", remove = F, extra = "drop", fill = "left")%>%
+    separate(funding_year, "funding_year_latest", sep = ",", remove = F, extra = "drop", fill = "left")%>%
+  mutate(year_founded = year(as_date(founded_year)),
+         year_funding = year(as_date(funding_year_latest)),
+         year_closed = year(as_date(closed_year)))
+}
+
+skimr::skim(tmp)
+
+
+cb_us_startups <- tmp %>% 
+  clean_cols()%>% 
+  group_by(funding_type_latest)%>%
+  count()
+
+# DEFINITIONS
+# https://support.crunchbase.com/hc/en-us/articles/115010458467-Glossary-of-Funding-Types
+
+
+define_startups <- function(df, start,end, last_fund){
+ temp <- df %>%
+    filter(isclosed == FALSE)%>%
+    filter(year_founded > start & year_founded < end) %>%
+    filter(year_funding > last_fund) %>%
+   filter(!is.na(funding_type_latest))%>%
+   filter(is.na(stock_symbol))
+  
+  print(paste0("share of total: ",nrow(temp)/nrow(df)))
+  return(temp)
+}
+
+# test run
+cb_us_startups <- tmp %>% 
+  clean_cols()%>%
+  define_startups(start = 2008,end = 2019, last_fund = 2013)
+
+skimr::skim(cb_us_startups)
+
 remove_outliers <- function(df,ubi_rm,div_rm){
   
   df %>%
