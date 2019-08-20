8eaab743bd7f3537ea3e147a449fb3703d94edfa
fansi-sifan <sifan1121@gmail.com>
2019-08-20 13:50:05 -0400

    update geocoding script

--- geocode.R
+++ geocode.R
@@ -1,28 +1,34 @@
-# GEOCODE
+# get a list of places to match --------
 
+unmatched <- unmatched %>%
+  mutate(add = paste0(city_name,", ",region_name))%>%
+  select(-afact1)
 
-# Geocode unmatched use geocoding api
+# GEOCODE=======================================
+# Geocode unmatched use geocoding api ----
 KEY <- Sys.getenv("GOOGLE_MAP_KEY")
+source("../R/code/add2FIPS.R")
 
-for (i in 1:nrow(unmatched)) {
-  unmatched$stcobk_fips[[i]] <- add2FIPS(unmatched$Headquarters.Location[[i]], KEY)
+for (i in 6:nrow(unmatched)) {
+  unmatched$stcobk_code[[i]] <- add2FIPS(unmatched$add[[i]], KEY)
+  
 }
 
 
-# update place list with stco_fips
+# update place list with stco_code -----
 place <- bind_rows(
   matched,
   unmatched %>%
-    mutate(stco_fips = str_sub(unmatched$stcobk_fips, 1, 5)) %>%
-    select(-stcobk_fips)
+    mutate(stco_code = str_sub(unmatched$stcobk_code, 1, 5)) %>%
+    select(-stcobk_code)
 )
 
-# update pl2fips xwalk
-pl2fips <- bind_rows(
+# update pl2fips xwalk ------
+pl2co <- bind_rows(
   pl2co,
   unmatched %>%
-    mutate(stco_fips = str_sub(unmatched$stcobk_fips, 1, 5)) %>%
-    select(-Headquarters.Location, -US)
+    mutate(stco_code = str_sub(unmatched$stcobk_code, 1, 5)) %>%
+    select(-city_name, -region_name, - add)
 )
 
-save(pl2fips, file = "V:/Sifan/SifanLiu/data/pl2fips.rda")
+save(pl2co, file = "V:/Sifan/SifanLiu/data/pl2co.rda")
