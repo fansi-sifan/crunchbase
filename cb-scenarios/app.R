e3d68b73c39aba8e92c51309189393c3510b008a
fansi-sifan <sifan1121@gmail.com>
2019-08-23 17:17:45 -0400

    shiny app

--- cb-scenarios/app.R
+++ cb-scenarios/app.R
@@ -1,131 +1,146 @@
 
+library(dplyr)
 library(shiny)
 library(plotly)
 library(DT)
+library(tidytext)
+library(visNetwork)
+library(igraph)
+library(reshape2)
 
 load("data/cbsa_100.rda")
-load("data/complexity_cbsa.rda")
-
-create_output <- function(df, itr) {
-  # Iterate 100 times to calculate KCI
-  for (i in itr) {
-    tmp <- df %>%
-      group_by(tech_name) %>%
-      mutate(ubi = sum(div) / ubi) %>%
-      ungroup() %>%
-      group_by(cbsa_name, cbsa_code) %>%
-      mutate(div = sum(ubi) / div) %>%
-      ungroup()
-    return(tmp)
-  }
-
-  KCI <- tmp %>%
-    select(cbsa_code, cbsa_name, KCI = div) %>%
-    unique()
-
-  TCI <- tmp %>%
-    select(tech_name, TCI = ubi) %>%
-    unique()
-
-  # merge
-  output <- df %>%
-    left_join(KCI, by = c("cbsa_code", "cbsa_name")) %>%
-    left_join(TCI, by = "tech_name") %>%
-    arrange(-KCI)
-
-  return(output)
-}
-
-
-plot_mean_ubi <- function(df) {
-  ggplot(
-    df %>%
-      group_by(cbsa_code, cbsa_name, div) %>%
-      summarise(mean_ubi = sum(ubi / div)),
-    aes(x = div, y = mean_ubi, label = cbsa_name)
-  ) +
-    geom_point(stat = "identity") +
-    geom_vline(aes(xintercept = mean(div)), color = "red") +
-    geom_hline(aes(yintercept = mean(mean_ubi)), color = "red")
-}
-
-
+load("data/cb_cbsa.rda")
+source("helper.R")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- navbarPage(
 
   # Application title
   titlePanel("metro technology complexity"),
+  tabPanel(
+    "complexity index",
+    # Sidebar with a slider input for number of
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("tag_min",
+          "(tech_us_total) Minimum number of companies for a tag to qualify as a category:",
+          min = 0,
+          max = 50,
+          value = 10
+        ),
 
-  # Sidebar with a slider input for number of
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("n",
-                  "Minimum number of companies to qualify as having relative technological advantage:",
-                  min = 1,
-                  max = 5,
-                  value = 3
+        sliderInput("tag_max",
+          "(tech_us_total) Maximum number of companies for a tag to qualify as a category:",
+          min = 1000,
+          max = 4500,
+          value = 2000
+        ),
+
+        htmlOutput("text"),
+
+        br(),
+
+        sliderInput("firm_n",
+          "(tech_msa_total) Minimum number of companies in a metro to qualify as having relative technological advantage:",
+          min = 0,
+          max = 5,
+          value = 3
+        ),
+
+        sliderInput("ubi",
+          "(Ubiquity) Minimum number of cities for a technology to qualify:",
+          min = 0,
+          max = 50,
+          value = 5
+        ),
+
+        sliderInput("div",
+          "(Diversity) Minimum number of technologies for a city to qualify:",
+          min = 0,
+          max = 50,
+          value = 5
+        ),
+
+        checkboxInput("msa",
+          "Top 100 metro only",
+          value = F
+        ),
+
+        h4("Metro diversity and average ubiquity of technologies"),
+        plotlyOutput("Plot")
       ),
-      
-      sliderInput("ubi",
-        "(Ubiquity) Minimum number of cities for a technology to qualify:",
-        min = 1,
-        max = 50,
-        value = 10
+
+      mainPanel(
+        h4("Metro startup Complexity Rank"),
+        DT::dataTableOutput("table_cbsa"),
+
+        h4("Metro - technology pairs"),
+        DT::dataTableOutput("table_all")
+      )
+    )
+  ),
+
+  tabPanel(
+    "Product Space",
+    fluidRow(
+      column(
+        2,
+        sliderInput("freq",
+          "Mininum number of co-occurance",
+          min = 5,
+          max = 10,
+          value = 8
+        ),
+        selectInput("msa_name",
+                    "Select a metro to highlight its product space",
+                    choices = "Select a metro")
       ),
-
-      sliderInput("div",
-        "(Diversity) Minimum number of technologies for a city to qualify:",
-        min = 1,
-        max = 50,
-        value = 5
-      ),
-
-      checkboxInput("msa",
-        "Top 100 metro only",
-        value = T
-      ),
-
-      h4("Metro diversity and average ubiquity of technologies"),
-      plotlyOutput("Plot")
-    ),
-
-    mainPanel(
-      h4("Metro startup Complexity Rank"),
-      DT::dataTableOutput("table_cbsa"),
-
-      h4("Metro - technology pairs (Search for metro or technology:"),
-      DT::dataTableOutput("table_all")
+      column(
+        10,
+        visNetworkOutput("network", height=800)
+      )
     )
   )
 )
 
 
 # Define server logic
-server <- function(input, output) {
-  data_final <- reactive(
-    complexity_cbsa %>%
-
-      # remove technologies only claimed by one city
-      filter(ubi >= input$ubi) %>%
-      # remove cities with low diversity
-      filter(div >= input$div) %>%
-      # remove nonmetros
-      # top 100 metros
-      {
-        if (input$msa) filter(., cbsa_code %in% cbsa_100) else filter(., !is.na(cbsa_code))
-      } %>%
-
-      ungroup() %>%
-      # recalculate diversity -------
-      group_by(cbsa_name, cbsa_code) %>%
-      mutate(div = n()) %>%
-      # recalculate ubiquity --------
-      group_by(tech_name) %>%
-      mutate(ubi = n()) %>%
-      ungroup() %>%
-      filter(n >= input$n)
+server <- function(input, output, session) {
+  data <- reactive(
+    cb_cbsa %>%
+      clean_cat(min = input$tag_min, max = input$tag_max)
   )
+
+  data_final <- reactive({
+    data() %>%
+      calculate_LQ() %>%
+      calculate_tci() %>%
+      remove_outliers(input$ubi, input$div, input$firm_n, input$msa) %>%
+      calculate_LQ_alt() %>%
+      calculate_tci() %>%
+      remove_outliers(input$ubi, input$div, input$firm_n, input$msa)
+  })
+  
+  observe({
+    updateSelectInput(session, "msa_name", choices = data_final()%>%select(cbsa_name)%>%unique())
+  })
+
+  nw_data <- reactive(
+    data_final() %>%
+      create_network(freq = input$freq, input$msa_name)
+  )
+
+
+  output$network <- renderVisNetwork(
+    nw_data() %>%
+      Plot_network()
+  )
+
+  output$text <- renderUI({
+    str1 <- paste0("Total unique categories: ", nrow(data() %>% select(tech_name) %>% unique()))
+    str2 <- paste0(nrow(cb_cbsa %>% select(permalink) %>% unique()) - nrow(data() %>% ungroup() %>% select(permalink) %>% unique()), " observations dropped")
+    HTML(paste(str1, str2, sep = "<br/>"))
+  })
 
   output$table_cbsa <- DT::renderDataTable(
     DT::datatable(create_output(data_final(), 100) %>%
@@ -136,10 +151,11 @@ server <- function(input, output) {
   )
 
   output$table_all <- DT::renderDataTable(
-    DT::datatable(create_output(data_final(), 100)%>%
-                    select(cbsa_name, tech_name, tech_msa_total = n, 
-                           TCI_city = div, TCI_tech = ubi, tech_us_total, tech_us_share, tech_msa_share, lq),
-                  filter = "top") %>%
+    DT::datatable(create_output(data_final(), 100) %>%
+      select(cbsa_name, tech_name,
+        tech_msa_total = n,
+        TCI_city = div, TCI_tech = ubi, tech_us_total, tech_us_share, tech_msa_share, lq
+      )) %>%
       formatRound(columns = c("tech_us_share", "tech_msa_share", "lq", "TCI_city", "TCI_tech"), digits = 2)
   )
 
---
+++ cb-scenarios/helper.R
@@ -0,0 +1,168 @@
+
+library(visNetwork)
+library(igraph)
+
+
+clean_cat <- function(df, min, max) {
+  df %>%
+    unnest_tokens(tech_name, cat_detail, token = "regex", pattern = ",") %>%
+    mutate(tech_name = trimws(tech_name)) %>%
+    group_by(tech_name) %>%
+    mutate(n = dplyr::n()) %>%
+    filter(n >= !!min) %>%
+    filter(n <= !!max)
+}
+
+
+calculate_LQ_alt <- function(df) {
+  df %>%
+    mutate(us_total = sum(n)) %>%
+    group_by(tech_name) %>%
+    mutate(
+      tech_us_total = sum(n),
+      tech_us_share = tech_us_total / us_total
+    ) %>%
+    group_by(cbsa_code, cbsa_name) %>%
+    mutate(tech_msa_share = n / sum(n)) %>%
+    mutate(lq = tech_msa_share / tech_us_share) %>%
+    arrange(-lq) %>%
+    ungroup()
+}
+
+calculate_LQ <- function(df) {
+  df %>%
+    group_by(cbsa_code, cbsa_name, tech_name) %>%
+    dplyr::count() %>%
+    ungroup() %>%
+    mutate(us_total = sum(n)) %>%
+    group_by(tech_name) %>%
+    mutate(
+      tech_us_total = sum(n),
+      tech_us_share = tech_us_total / us_total
+    ) %>%
+    group_by(cbsa_code, cbsa_name) %>%
+    mutate(tech_msa_share = n / sum(n)) %>%
+    mutate(lq = tech_msa_share / tech_us_share) %>%
+    arrange(-lq) %>%
+    ungroup()
+}
+
+calculate_tci <- function(df) {
+  df %>%
+    # a place has Relative Technological Advantage (RTA) when LQ > 1
+    filter(lq > 1) %>%
+
+    # calculate diversity -------
+    group_by(cbsa_name, cbsa_code) %>%
+    mutate(div = dplyr::n()) %>%
+    # calculate ubiquity --------
+    group_by(tech_name) %>%
+    mutate(ubi = dplyr::n()) %>%
+    ungroup()
+}
+
+remove_outliers <- function(df, ubi_rm, div_rm, firm_n, msa = TRUE) {
+  df %>%
+
+    # remove technologies only claimed by one city
+    filter(ubi >= !!ubi_rm) %>%
+    # remove cities with low diversity
+    filter(div >= !!div_rm) %>%
+
+    # remove nonmetros
+    {
+      if (msa) filter(., cbsa_code %in% cbsa_100) else filter(., !is.na(cbsa_code))
+    } %>%
+    ungroup() %>%
+    filter(n > !!firm_n) %>%
+    # recalculate diversity -------
+    group_by(cbsa_name, cbsa_code) %>%
+    mutate(div = dplyr::n()) %>%
+    # recalculate ubiquity --------
+    group_by(tech_name) %>%
+    mutate(ubi = dplyr::n()) %>%
+    ungroup()
+}
+
+create_output <- function(df, itr) {
+  # Iterate 100 times to calculate KCI
+  for (i in itr) {
+    tmp <- df %>%
+      group_by(tech_name) %>%
+      mutate(ubi = sum(div) / ubi) %>%
+      ungroup() %>%
+      group_by(cbsa_name, cbsa_code) %>%
+      mutate(div = sum(ubi) / div) %>%
+      ungroup()
+    return(tmp)
+  }
+
+  KCI <- tmp %>%
+    select(cbsa_code, cbsa_name, KCI = div) %>%
+    unique()
+
+  TCI <- tmp %>%
+    select(tech_name, TCI = ubi) %>%
+    unique()
+
+  # merge
+  output <- df %>%
+    left_join(KCI, by = c("cbsa_code", "cbsa_name")) %>%
+    left_join(TCI, by = "tech_name") %>%
+    arrange(-KCI)
+
+  return(output)
+}
+
+
+plot_mean_ubi <- function(df) {
+  ggplot(
+    df %>%
+      group_by(cbsa_code, cbsa_name, div) %>%
+      summarise(mean_ubi = sum(ubi / div)),
+    aes(x = div, y = mean_ubi, label = cbsa_name)
+  ) +
+    geom_point(stat = "identity") +
+    geom_vline(aes(xintercept = mean(div)), color = "red") +
+    geom_hline(aes(yintercept = mean(mean_ubi)), color = "red")
+}
+
+
+create_network <- function(df, freq, metro_name) {
+  tech_long <- df %>%
+    select(cbsa_name, tech_name, n)
+
+  w <- tech_long %>%
+    reshape2::dcast(tech_name ~ cbsa_name)
+
+  x <- as.matrix(w[, -1])
+  x[is.na(x)] <- 0
+  x <- apply(x, 2, function(x) as.numeric(x > 0)) # recode as 0/1
+  v <- x %*% t(x) # the magic matrix
+  # d <- diag(v)
+  diag(v) <- 0 # repalce diagonal
+  dimnames(v) <- list(w[, 1], w[, 1]) # name the dimensions
+
+  # proximity ---
+  # t <- v / d # min?
+
+  # test.gr <- igraph::graph_from_adjacency_matrix(v, mode = "undirected", weighted = T)
+  edges <-  as.data.frame(as.table(v)) %>% filter(Freq >!!freq)
+  colnames(edges) <- c("from", "to", "value")
+    
+  # test.visn <- toVisNetworkData(test.gr)
+  msa_space <- (df%>%filter(cbsa_name==!!metro_name))$tech_name
+  nodes <- bind_rows(edges%>%select(id=from),
+                 edges%>%select(id=to))%>%
+    unique()%>%
+    mutate(label = id,
+           color.background = ifelse(label %in% msa_space,"blue","grey"))
+  
+  return(list("edges"=edges,"nodes"=nodes))
+}
+
+Plot_network <- function(nw) {
+  # plot(test.gr)
+  visNetwork(nw$nodes, nw$edges) %>%
+    visIgraphLayout()
+}
