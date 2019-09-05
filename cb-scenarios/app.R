304444658137eb23b50fdf1d2c4ffbab3e0dee13
fansi-sifan <sifan1121@gmail.com>
2019-09-05 16:02:25 -0400

    add options to choose RCA method

--- cb-scenarios/app.R
+++ cb-scenarios/app.R
@@ -1,5 +1,6 @@
 
 library(dplyr)
+library(purrr)
 library(shiny)
 library(plotly)
 library(DT)
@@ -7,9 +8,13 @@ library(tidytext)
 library(visNetwork)
 library(igraph)
 library(reshape2)
+library(rsample)
 
 load("data/cbsa_100.rda")
 load("data/cb_cbsa.rda")
+cb_cbsa <- cb_cbsa %>%
+  filter(!is.na(cbsa_code))
+
 source("helper.R")
 
 # Define UI for application that draws a histogram
@@ -33,12 +38,13 @@ ui <- navbarPage(
           "(tech_us_total) Maximum number of companies for a tag to qualify as a category:",
           min = 1000,
           max = 4500,
-          value = 2000
+          value = 4000
         ),
-
+     
         htmlOutput("text"),
-
+        
         br(),
+        
 
         sliderInput("firm_n",
           "(tech_msa_total) Minimum number of companies in a metro to qualify as having relative technological advantage:",
@@ -46,25 +52,44 @@ ui <- navbarPage(
           max = 5,
           value = 3
         ),
+   
 
-        sliderInput("ubi",
-          "(Ubiquity) Minimum number of cities for a technology to qualify:",
-          min = 0,
-          max = 50,
-          value = 5
+        selectInput(
+          inputId = "method",
+          label = "Choose a parameter to measure RCA: ",
+          choices = c(
+            "Location quotient (lq)"  = "lq",
+            # "Normalized location quotient, unweighted (slq)" = "slq",
+            "Normalized location quotient, weighted (SLQ)" = "SLQ"
+          ),
+          selected = "SLQ"
         ),
 
-        sliderInput("div",
-          "(Diversity) Minimum number of technologies for a city to qualify:",
-          min = 0,
-          max = 50,
-          value = 5
+        sliderInput("probs",
+          "Choose a position in distribution to determine RCA",
+          min = 0.5,
+          max = 1,
+          value = 0.95
         ),
 
-        checkboxInput("msa",
-          "Top 100 metro only",
-          value = F
-        ),
+        # sliderInput("ubi",
+        #   "(Ubiquity) Minimum number of cities for a technology to qualify:",
+        #   min = 0,
+        #   max = 50,
+        #   value = 5
+        # ),
+        #
+        # sliderInput("div",
+        #   "(Diversity) Minimum number of technologies for a city to qualify:",
+        #   min = 0,
+        #   max = 50,
+        #   value = 5
+        # ),
+        #
+        # checkboxInput("msa",
+        #   "Top 100 metro only",
+        #   value = F
+        # ),
 
         h4("Metro diversity and average ubiquity of technologies"),
         plotlyOutput("Plot")
@@ -88,16 +113,17 @@ ui <- navbarPage(
         sliderInput("freq",
           "Mininum number of co-occurance",
           min = 5,
-          max = 10,
-          value = 8
+          max = 20,
+          value = 10
         ),
         selectInput("msa_name",
-                    "Select a metro to highlight its product space",
-                    choices = "Select a metro")
+          "Select a metro to highlight its product space",
+          choices = "Select a metro"
+        )
       ),
       column(
         10,
-        visNetworkOutput("network", height=800)
+        visNetworkOutput("network", height = 800)
       )
     )
   )
@@ -108,21 +134,30 @@ ui <- navbarPage(
 server <- function(input, output, session) {
   data <- reactive(
     cb_cbsa %>%
-      clean_cat(min = input$tag_min, max = input$tag_max)
+      clean_cat(min = input$tag_min, max = input$tag_max) 
   )
 
   data_final <- reactive({
-    data() %>%
+    tmp <- data()%>%
+      clean_firms(input$firm_n)%>%
       calculate_LQ() %>%
-      calculate_tci() %>%
-      remove_outliers(input$ubi, input$div, input$firm_n, input$msa) %>%
-      calculate_LQ_alt() %>%
-      calculate_tci() %>%
-      remove_outliers(input$ubi, input$div, input$firm_n, input$msa)
+      calculate_SLQ() 
+    
+    tmp %>%
+      left_join(get_SLLQ(tmp, "SLQ", input$probs), by = "tech_name") %>%
+      rename(z_SLQ = value) %>%
+      left_join(get_SLLQ(tmp, "slq", input$probs), by = "tech_name") %>%
+      rename(z_slq = value) %>%
+      ungroup() %>%
+      calculate_tci(method = input$method)
   })
-  
+
+  # network
   observe({
-    updateSelectInput(session, "msa_name", choices = data_final()%>%select(cbsa_name)%>%unique())
+    updateSelectInput(session, "msa_name", 
+                      choices = data_final() %>% 
+                        select(cbsa_name) %>% 
+                        unique())
   })
 
   nw_data <- reactive(
@@ -136,6 +171,8 @@ server <- function(input, output, session) {
       Plot_network()
   )
 
+  # data table
+
   output$text <- renderUI({
     str1 <- paste0("Total unique categories: ", nrow(data() %>% select(tech_name) %>% unique()))
     str2 <- paste0(nrow(cb_cbsa %>% select(permalink) %>% unique()) - nrow(data() %>% ungroup() %>% select(permalink) %>% unique()), " observations dropped")
@@ -143,7 +180,7 @@ server <- function(input, output, session) {
   })
 
   output$table_cbsa <- DT::renderDataTable(
-    DT::datatable(create_output(data_final(), 100) %>%
+    DT::datatable(create_output(data_final(), 900) %>%
       select(cbsa_name, TCI_city = div) %>%
       arrange(-TCI_city) %>%
       unique()) %>%
@@ -151,12 +188,13 @@ server <- function(input, output, session) {
   )
 
   output$table_all <- DT::renderDataTable(
-    DT::datatable(create_output(data_final(), 100) %>%
+    DT::datatable(create_output(data_final(), 900) %>%
       select(cbsa_name, tech_name,
         tech_msa_total = n,
-        TCI_city = div, TCI_tech = ubi, tech_us_total, tech_us_share, tech_msa_share, lq
+        TCI_city = div, TCI_tech = ubi, tech_us_total, tech_us_share, tech_msa_share, lq, LQ, slq, SLQ
       )) %>%
-      formatRound(columns = c("tech_us_share", "tech_msa_share", "lq", "TCI_city", "TCI_tech"), digits = 2)
+      formatPercentage(columns = c("tech_us_share", "tech_msa_share"), digits = 2 )%>%
+      formatRound(columns = c("TCI_city", "TCI_tech", "lq","LQ","slq", "SLQ"), digits = 2)
   )
 
   output$Plot <- renderPlotly({
