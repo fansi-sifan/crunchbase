b5aff0bf38b61abcaf45b653d90eaea6c780e7f2
fansi-sifan <sifan1121@gmail.com>
2019-12-31 13:23:58 -0500

    add gender

--- 0_fetch.R
+++ 0_fetch.R
@@ -22,6 +22,12 @@ query <- paste0(base,
 df <- fromJSON(query)
 tmp <- df$data$items$properties %>% as.data.frame()
 
+query <- paste0( "https://api.crunchbase.com/v3.1/people?", "&user_key=", key)
+
+df <- fromJSON(query)
+t <- df$data$items$properties %>% as.data.frame()
+t$gender
+
 # # FETCh all data via continuous pagniation -----------------------------
 while (!is.null(df$data$paging$key_set_url)){
 
@@ -41,7 +47,8 @@ save(tmp, file = "cb_us_companies_122319.rda")
 
 # WARNINGS: takes about 50 hours
 
-rel <- "categories,funding_rounds,acquisitions"
+# rel <- "categories,funding_rounds"
+rel <-  "founders"
 
 # fetch information
 get_data <- function(data) {
@@ -51,9 +58,6 @@ get_data <- function(data) {
     paste(data, collapse = ", ")
   }
 }
-
-tmp <- tmp %>%
-  mutate(cat_detail = NA, isclosed = NA, founded_year = NA, closed_year = NA, funding_year = NA, funding_type = NA)
 
 for (i in 1:nrow(tmp)) {
   t <- try({
@@ -71,14 +75,20 @@ for (i in 1:nrow(tmp)) {
     next
   }
   else {
-    tmp$cat_detail[[i]] <- get_data(df$data$relationships$categories$items$properties$name)
-
-    tmp$founded_year[i] <- get_data(df$data$properties$founded_on)
-    tmp$isclosed[i] <- get_data(df$data$properties$is_closed)
-    tmp$closed_year[i] <- get_data(df$data$properties$closed_on)
-
-    tmp$funding_year[[i]] <- get_data(df$data$relationships$funding_rounds$items$properties$announced_on)
-    tmp$funding_type[[i]] <- get_data(df$data$relationships$funding_rounds$items$properties$funding_type)
+    
+    tmp$founder_firstname[[i]] <- get_data(df$data$relationships$founders$items$properties$first_name)
+    tmp$founder_lastname[[i]] <- get_data(df$data$relationships$founders$items$properties$last_name)
+    tmp$founder_gender[[i]] <- get_data(df$data$relationships$founders$items$properties$gender)
+    
+    # 
+    # tmp$cat_detail[[i]] <- get_data(df$data$relationships$categories$items$properties$name)
+    # 
+    # tmp$founded_year[i] <- get_data(df$data$properties$founded_on)
+    # tmp$isclosed[i] <- get_data(df$data$properties$is_closed)
+    # tmp$closed_year[i] <- get_data(df$data$properties$closed_on)
+    # 
+    # tmp$funding_year[[i]] <- get_data(df$data$relationships$funding_rounds$items$properties$announced_on)
+    # tmp$funding_type[[i]] <- get_data(df$data$relationships$funding_rounds$items$properties$funding_type)
   }
   
   if (i %% 1000 == 0) {
