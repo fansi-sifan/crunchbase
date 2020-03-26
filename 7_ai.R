dcc5c756b43db79b28ae37af56da743ec87be350
fansi-sifan <sifan1121@gmail.com>
2020-03-26 15:36:02 -0400

    ai companies on cb

--- 7_ai.R
+++ 7_ai.R
@@ -1,16 +1,42 @@
 # ai categories
 library(tidyverse)
-ai_cb_cat <- c("artificial intelligence", "machine learning", "deep learning", "neural networks", "robotics", "face recognition", "image processing", "computer vision", "speech recognition", "natural language processing", "autonomous driving", "autonomous vehicle", "semantic web")
+library(tidylog)
+
+source("1_fclean.R")
+source("cb-scenarios/helper.R")
+
+ai_cb_cat <- c("artificial intelligence", "machine learning", "deep learning", 
+               "neural networks", "robotics", "face recognition", "image processing", 
+               "computer vision", "speech recognition", "natural language processing", 
+               "autonomous driving", "autonomous vehicle", "semantic web", "image recognition")
 
 load("cb_us_companies_122319_details.rda")
 
+# tokenize, remove rare categories
 df_ai <- tmp %>%
-  clean_cat(min = 10, max = 10000) 
+  clean_cols() %>%
+  clean_cat(min = 5, max = Inf) %>%
+  # companies that are still operating
+  filter(isclosed == "FALSE") %>%
+  # year companies founded
+  # filter(year_founded > start & year_founded < end) %>%
+  # # companies received at least one funding
+  # filter(!is.na(funding_type_latest)) %>%
+  # # year companies received last funding
+  # filter(year_funding > last_fund) %>%
+  # # companies that has not gone IPO
+  # filter(is.na(stock_symbol))
+  mutate(ai = str_detect(tech_name, paste(ai_cb_cat, collapse = "|"))) 
+
 
 df_ai %>%
-  mutate(ai = str_detect(tech_name, paste(ai_cb_cat, collapse = "|"))) %>%
+  filter(ai)%>%
   ungroup()%>%
-  select(-tech_name) %>%
-  unique()%>%
   skimr::skim()
+
+
+df_ai %>%
+  group_by(permalink) %>%
+  summarise(ai = sum(ai, na.rm = T)) %>%
+  count(ai > 0)
 
